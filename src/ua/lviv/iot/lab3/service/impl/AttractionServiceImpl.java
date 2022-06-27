83dc74225e80f4b9fab377286b68ec20fdc1de39
ihor-patko <91693589+ihor-patko@users.noreply.github.com>
2022-06-27 19:17:44 +0300

    Update AttractionServiceImpl.java

--- src/ua/lviv/iot/lab3/service/impl/AttractionServiceImpl.java
+++ src/ua/lviv/iot/lab3/service/impl/AttractionServiceImpl.java
@@ -2,14 +2,12 @@ package ua.lviv.iot.lab3.service.impl;
 
 import ua.lviv.iot.lab3.entity.Attractions;
 import ua.lviv.iot.lab3.service.AttractionService;
-
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class AttractionServiceImpl implements AttractionService {
-
     @Override
     public List<Attractions> sortByPermissionABS(List<Attractions> attractionsList, String permission) {
         List<Attractions> sorted = attractionsList.stream().sorted(Comparator.comparing(Attractions::getCost)).collect(Collectors.toList());
@@ -20,7 +18,6 @@ public class AttractionServiceImpl implements AttractionService {
                 .collect(Collectors.toList());
         return result;
     }
-
     @Override
     public List<Attractions> sortByPermissionDESC(List<Attractions> attractionsList, String permission) {
         List<Attractions> reverse = attractionsList.stream().sorted(Collections.reverseOrder(Comparator.comparing(Attractions::getCost))).collect(Collectors.toList());
@@ -31,13 +28,11 @@ public class AttractionServiceImpl implements AttractionService {
                 .collect(Collectors.toList());
         return result;
     }
-
     @Override
     public List<Attractions> sortByNumberKidsABS(List<Attractions> attractionsList) {
         attractionsList.stream().max(Comparator.comparing(Attractions::getSeat));
         return attractionsList;
     }
-
     @Override
     public List<Attractions> sortByNumberKidsDESC(List<Attractions> attractionsList) {
         attractionsList.stream().min(Comparator.comparing(Attractions::getSeat));

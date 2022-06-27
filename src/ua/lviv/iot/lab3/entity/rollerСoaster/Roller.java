aa058d66b2349335c8cfff9a2e4a541339c95645
ihor-patko <91693589+ihor-patko@users.noreply.github.com>
2022-06-27 19:18:21 +0300

    Update Roller.java

--- src/ua/lviv/iot/lab3/entity/rollerСoaster/Roller.java
+++ src/ua/lviv/iot/lab3/entity/rollerСoaster/Roller.java
@@ -4,15 +4,10 @@ import ua.lviv.iot.lab3.entity.Attractions;
 
 public class Roller extends Attractions {
     private String name;
-
     private int length;
-
     public Roller(String kind, int cost, String permission, int seat, String name,int length) {
         super(kind, cost, permission, seat);
         this.name = name;
         this.length = length;
-
     }
-
-
 }
